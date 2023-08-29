d28673fdc17e904ba60dab7dbb2ac7f827e1558e
Thomas <tbe@pm.me>
2023-08-29 13:42:08 +0200

    prepared API on index.js

--- backend/Middleware/sendMdc.js
+++ backend/Middleware/sendMdc.js
@@ -1,28 +1,28 @@
 const Net = require('net');
 
-function sendRj(num, hosts, port, hex) {
+function sendRj(host, port, hex) {
     setTimeout(function() {
         //console.log('Starting ', num, hosts[num], port );
 		let obj = 'client' + num;
 		//console.log(obj);
 		obj = new Net.Socket();
-		obj.connect({ port: port, host: hosts[num] } , () => {
-		console.log(`TCP connection established with the screen ${hosts[num]} `);
-		setTimeout( ()=> {
-			obj.write(hex,  () => {
-				obj.destroy();	
-			   //console.log('wrote.')			
-		 		});
-		 	},200)			
+		obj.connect({ port: port, host: host } , () => {
+			console.log(`TCP connection established with the screen ${host} `);
+			setTimeout( ()=> {
+				obj.write(hex,  () => {
+					obj.destroy();	
+				//console.log('wrote.')			
+						});
+				},200)			
 		 });
 		//console.log('Stopping ', num );
     }, 200);
-}
+};
 
 //just for Test, to remove afterwards
-const paneloffToSend = [0xAA, 0xF9, 0xFE, 0x01, 0x00, 0xF8]
-var paneloffhex = new Uint8Array(paneloffToSend);
-let cmd = Buffer.from(paneloffToSend)
+// const paneloffToSend = [0xAA, 0xF9, 0xFE, 0x01, 0x00, 0xF8]
+// var paneloffhex = new Uint8Array(paneloffToSend);
+// let cmd = Buffer.from(paneloffToSend)
 
 
 //sendCode(paneloffhex)
